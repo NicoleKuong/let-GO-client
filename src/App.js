0a61e55cab090abe7bec007cb605ae25b416b5a8
NicoleKuong <nicolekuong@gmail.com>
2020-03-04 14:45:40 +0100

    make routes for signin and login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
-
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
+import SignUpContainer from "./components/SignUp/SignUpContainer";
+import LoginContainer from "./components/Login/LoginContainer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/signup" component={SignUpContainer} />
+          <Route exact path="/login" component={LoginContainer} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
